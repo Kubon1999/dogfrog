f6630b9169584f7d7196abe4c5580bf69eab4c00
Kubon1999 <kuba.wolodzko@gmail.com>
2025-11-17 15:00:12 +0100

    add CA

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 						onClick={handleCopy}
 						className="px-3 py-1 border border-white rounded hover:bg-white hover:text-black transition"
 					>
-						{copied ? 'Copied!' : 'soon'}
+						{copied ? 'Copied!' : 'CA: soon'}
 					</button>
 				</div>
 			</div>
